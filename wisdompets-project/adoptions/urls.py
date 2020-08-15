1b5933a96dd3192c8dde469f3a65ec4d8eb7af24
Kyasanku Charles Nyombi <43342042+Kyasanku@users.noreply.github.com>
2020-08-15 13:51:56 +0300

    Created url patterns file for the adoptions app

---
+++ wisdompets-project/adoptions/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = "adoptions"
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('adoptions/<int:pet_id>/', views.pet_detail, name='pet_detail'),
+]
